1be093282a20cb2896a7b87b5237a9d23fe3e944
Claudio Carnino <carninoclaudio@gmail.com>
2014-10-22 17:20:39 +0100

    Renamed the Module

---
+++ RandomColor/RandomColor.h
@@ -0,0 +1,19 @@
+//
+//  RandomColor.h
+//  RandomColor
+//
+//  Created by Claudio Carnino on 22/10/2014.
+//  Copyright (c) 2014 Tugulab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RandomColor.
+FOUNDATION_EXPORT double RandomColorVersionNumber;
+
+//! Project version string for RandomColor.
+FOUNDATION_EXPORT const unsigned char RandomColorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RandomColor/PublicHeader.h>
+
+
--- RandomColor/ViewController.swift
+++ RandomColor/ViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-import RandomColorLib
+import RandomColor
 
 class ViewController: UIViewController {
 
